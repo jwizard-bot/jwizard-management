831ad2a14bca4496a5fa5894621c4c99a81877c7
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-04-11 18:30:52 +0200

    (refactor) add barrel import to router package

--- jwm-client/src/index.tsx
+++ jwm-client/src/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { SuspenseWrapper } from '@/component';
 import { ReduxStoreWrapper } from '@/redux';
+import { AppRouter } from '@/router';
 import { AppInitiator } from '@/router/app-initiator';
-import { AppRouter } from '@/router/app-router';
 import { MuiThemeSupplier } from '@/util/mui-theme-supplier';
 
 const rootElement = document.getElementById('app-mount') as HTMLElement;
