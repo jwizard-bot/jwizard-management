b1a03ce44df7178d9a599fbf1305090ae293ae3f
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-04-16 02:15:47 +0200

    (refactor) remove unused duplicated name property

--- jwm-server/src/main/kotlin/pl/jwizard/jwm/server/core/exception/SpecifiedException.kt
+++ jwm-server/src/main/kotlin/pl/jwizard/jwm/server/core/exception/SpecifiedException.kt
@@ -3,14 +3,14 @@ package pl.jwizard.jwm.server.core.exception
 import io.javalin.http.HttpStatus
 import pl.jwizard.jwl.server.exception.ExceptionBase
 
-enum class SpecifiedException(
-	override val key: String,
-	override val httpStatus: HttpStatus
-) : ExceptionBase {
-	INCORRECT_USERNAME_AND_OR_PASSWORD("INCORRECT_USERNAME_AND_OR_PASSWORD", HttpStatus.UNAUTHORIZED),
-	INCORRECT_MFA_TOKEN("INCORRECT_MFA_TOKEN", HttpStatus.UNAUTHORIZED),
-	INCORRECT_RECOVERY_CODE("INCORRECT_RECOVERY_CODE", HttpStatus.UNAUTHORIZED),
-	UNABLE_TO_CHANGE_DEFAULT_PASSWORD("UNABLE_TO_CHANGE_DEFAULT_PASSWORD", HttpStatus.BAD_REQUEST),
-	SESSION_BASED_ID_NOT_FOUND("SESSION_BASED_ID_NOT_FOUND", HttpStatus.NOT_FOUND),
+enum class SpecifiedException(override val httpStatus: HttpStatus) : ExceptionBase {
+	INCORRECT_USERNAME_AND_OR_PASSWORD(HttpStatus.UNAUTHORIZED),
+	INCORRECT_MFA_TOKEN(HttpStatus.UNAUTHORIZED),
+	INCORRECT_RECOVERY_CODE(HttpStatus.UNAUTHORIZED),
+	UNABLE_TO_CHANGE_DEFAULT_PASSWORD(HttpStatus.BAD_REQUEST),
+	SESSION_BASED_ID_NOT_FOUND(HttpStatus.NOT_FOUND),
 	;
+
+	override val key
+		get() = name
 }
