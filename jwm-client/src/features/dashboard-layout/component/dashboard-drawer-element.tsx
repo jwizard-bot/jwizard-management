374f0a3ea822cbd3f9569a1f2fa016c9f5026a11
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-04-21 22:51:53 +0200

    (fix) on mobile devices close drawer after click link

--- jwm-client/src/features/dashboard-layout/component/dashboard-drawer-element.tsx
+++ jwm-client/src/features/dashboard-layout/component/dashboard-drawer-element.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { Dispatch, useCallback, useMemo } from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { useMainSlice } from '@/redux/store/main-slice';
+import { useIsMobile } from '@/hook/use-is-mobile';
+import { setDashboardDrawerState, useMainSlice } from '@/redux/store/main-slice';
 import { ExpandLess as ExpandLessIcon, ExpandMore as ExpandMoreIcon } from '@mui/icons-material';
 import { Collapse, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { MenuItem } from '../config/menu-items';
@@ -20,7 +22,17 @@ const DashboardDrawerElement: React.FC<Props> = ({
   setOpenSubMenu,
 }): React.ReactElement | null => {
   const { loggedUser } = useMainSlice();
+  const dispatch = useDispatch();
+  const isMobile = useIsMobile();
+
   const hasSubitems = subItems && subItems.length > 0;
+
+  const onCloseDrawer = (): void => {
+    if (isMobile) {
+      // close drawer only on mobile
+      dispatch(setDashboardDrawerState(false));
+    }
+  };
 
   const allForAdmin = useMemo(
     () => subItems?.every(({ onlyForAdmin }) => !!onlyForAdmin) || onlyForAdmin,
@@ -49,7 +61,7 @@ const DashboardDrawerElement: React.FC<Props> = ({
 
   if (!hasSubitems) {
     return (
-      <ListItemButton component={Link} to={path}>
+      <ListItemButton component={Link} to={path} onClick={onCloseDrawer}>
         <ListItemIcon>{icon}</ListItemIcon>
         <ListItemText primary={text} />
       </ListItemButton>
@@ -66,7 +78,12 @@ const DashboardDrawerElement: React.FC<Props> = ({
       <Collapse in={openSubMenu[index]} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
           {filteredSubItemsForAdmin.map((subItem, subIndex) => (
-            <ListItemButton key={subIndex} component={Link} to={path + subItem.path} sx={{ pl: 4 }}>
+            <ListItemButton
+              key={subIndex}
+              component={Link}
+              onClick={onCloseDrawer}
+              to={path + subItem.path}
+              sx={{ pl: 4 }}>
               <ListItemText primary={subItem.text} />
             </ListItemButton>
           ))}
